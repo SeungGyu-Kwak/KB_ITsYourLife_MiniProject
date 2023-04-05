a46b3f483f38d4c7bae227355962954edf5aa966
Gyu <tmdrb0912@naver.com>
2023-04-05 10:54:51 +0900

    feat : 구현중!

--- KB_ITsYourLife_MiniProject/src/cafe/dao/CoffeeDAO.java
+++ KB_ITsYourLife_MiniProject/src/cafe/dao/CoffeeDAO.java
@@ -1,14 +1,24 @@
 package cafe.dao;
 
 import java.util.List;
+
 import cafe.dto.CoffeeDto;
+import cafe.exception.SearchWrongException;
+
 
 public interface CoffeeDAO {
 	/**
 	 * 커피 전체 정보검색하기
 	 * select * from coffee;
 	 * */
-	public List<CoffeeDto> selectAll();
+	public List<CoffeeDto> selectAll() throws SearchWrongException;
+	
+	
+	/**
+	 * 커피 메뉴에 특정 문자열이 포함된 레코드 검색 - 0개이상의 레코드 리턴!!!
+	 * select * from coffee where subject like ?
+	 */
+	List<CoffeeDto> coffeeSelectByName(String keyWord) throws SearchWrongException;
 	
 
 }
--- KB_ITsYourLife_MiniProject/src/cafe/dao/CoffeeDAOImpl.java
+++ KB_ITsYourLife_MiniProject/src/cafe/dao/CoffeeDAOImpl.java
@@ -3,11 +3,18 @@ package cafe.dao;
 import java.util.List;
 
 import cafe.dto.CoffeeDto;
+import cafe.exception.SearchWrongException;
 
 public class CoffeeDAOImpl implements CoffeeDAO{
 
 	@Override
 	public List<CoffeeDto> selectAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<CoffeeDto> coffeeSelectByName(String keyWord) throws SearchWrongException {
 		// TODO Auto-generated method stub
 		return null;
 	}
