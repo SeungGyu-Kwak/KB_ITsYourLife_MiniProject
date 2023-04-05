959734ba5fe2d57bbacc391f6de137a164a51b46
Gyu <tmdrb0912@naver.com>
2023-04-05 13:23:23 +0900

    feat : CafeService 인터페이스 작성

--- KB_ITsYourLife_MiniProject/src/cafe/service/CafeService.java
+++ KB_ITsYourLife_MiniProject/src/cafe/service/CafeService.java
@@ -1,5 +1,19 @@
 package cafe.service;
 
+import java.util.List;
+
+import cafe.dto.CoffeeDto;
+import cafe.exception.SearchWrongException;
+
 public interface CafeService {
 
+	/**
+	 * 모든 커피 메뉴 검색
+	 */
+	List<CoffeeDto> selectAll() throws SearchWrongException;
+	
+	/**
+	 * 커피메뉴에 키워드 입력해서 상세정보 보기
+	 * */
+	List<CoffeeDto> coffeeSelectByName(String keyWord) throws SearchWrongException;
 }
