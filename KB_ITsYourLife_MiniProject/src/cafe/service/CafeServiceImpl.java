5e3977ff3089c51278d7c336da3aba39bc0787ab
somin <aceannie@naver.com>
2023-04-05 13:59:59 +0900

    test

--- KB_ITsYourLife_MiniProject/src/cafe/service/CafeServiceImpl.java
+++ KB_ITsYourLife_MiniProject/src/cafe/service/CafeServiceImpl.java
@@ -1,5 +1,31 @@
 package cafe.service;
 
-public class CafeServiceImpl {
+import java.util.List;
+
+import cafe.dto.CoffeeDto;
+import cafe.exception.SearchWrongException;
+
+public class CafeServiceImpl implements CafeService{
+	
+	private static CafeService instance = new CafeServiceImpl();
+	private CoffeeDAO coffeeDAO = CoffeeDAOImpl.getInstance();
+	
+	private CafeServiceImpl() {}
+	public static CafeService getInstance() {
+		return instance;
+	}
+	
+	
+	@Override
+	public List<CoffeeDto> selectAll() throws SearchWrongException {
+		
+		return null;
+	}
+
+	@Override
+	public List<CoffeeDto> coffeeSelectByName(String keyWord) throws SearchWrongException {
+		
+		return null;
+	}
 
 }
