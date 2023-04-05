027974a40c22efa36d4e63d1b34070d15b1c985f
Gyu <tmdrb0912@naver.com>
2023-04-05 14:07:38 +0900

    feat : controller, successView 구현

--- KB_ITsYourLife_MiniProject/src/cafe/controller/CafeController.java
+++ KB_ITsYourLife_MiniProject/src/cafe/controller/CafeController.java
@@ -1,5 +1,33 @@
 package cafe.controller;
 
+import java.util.List;
+
+import cafe.dto.CoffeeDto;
+import cafe.exception.SearchWrongException;
+import cafe.service.CafeService;
+import cafe.service.CafeServiceImpl;
+import cafe.view.FailView;
+import cafe.view.SuccessView;
+
 public class CafeController {
 
+	private static CafeService cafeService = CafeServiceImpl.getInstance();
+	
+	/**
+	 * 카페메뉴 전체 검색
+	 * @작성자 : 윤소민
+	 * */
+	
+	/**
+	 * 카페메뉴 이름 쳐서 해당 메뉴 상세 정보 보기
+	 * @작성자 : 곽승규
+	 * */
+	public static void coffeeSelectByName(String keyWord) {
+		try {
+			List<CoffeeDto>coffeeList = cafeService.coffeeSelectByName(keyWord);
+			SuccessView.selectByNamePrint(coffeeList, keyWord);
+		} catch (SearchWrongException e) {
+			FailView.errorMessage(e.getMessage());
+		}
+	}
 }
--- KB_ITsYourLife_MiniProject/src/cafe/view/SuccessView.java
+++ KB_ITsYourLife_MiniProject/src/cafe/view/SuccessView.java
@@ -2,32 +2,35 @@ package cafe.view;
 
 import java.util.List;
 
+import cafe.dto.CoffeeDto;
+
 public class SuccessView {
 	
-//	public static void selectPrint(List<> list) {
-//		System.out.println("----Board LIST ("+list.size()+") 개 ------------------");
-//		for( : list) {
-//			System.out.println();//board.toString()호출
-//		}
-//		
-//	}
-//
-//	public static void messagePrint(String message) {
-//		System.out.println(message);
-//		
-//	}
-//
-//	public static void selectByNoPrint(BoardDTO boardDTO) {
-//		System.out.println(boardDTO);
-//		
-//	}
-//
-//	public static void selectReplyPrint(BoardDTO boardDTO) {
-//		System.out.println(boardDTO);
-//		System.out.println("댓글 개수 : " + boardDTO.getRepliesList().size() +"개 ---------");
-//		for(ReplyDTO reply : boardDTO.getRepliesList()) {
-//			System.out.println("       ▶ " + reply );
-//		}
-//		
-//	}
+	
+	/**
+	 * 카페 전체 메뉴 보여주는 메소드
+	 * */
+	public static void selectPrint(List<CoffeeDto> list) {
+		System.out.println("----Menu LIST ("+list.size()+") 개 ------------------");
+		for(CoffeeDto menu : list) {
+			System.out.println(menu);//board.toString()호출
+		}
+		
+	}
+
+	public static void messagePrint(String message) {
+		System.out.println(message);
+	}
+
+	/**
+	 * 카페 메뉴 중 키워드 검색해서 나온 결과 보여주는 메소드
+	 * @작성자 : 곽승규
+	 * */
+	public static void selectByNamePrint(List<CoffeeDto> list, String keyword){
+		System.out.println("------ 해당 키워드 " + keyword + " 검색결과(" + list.size() + "개) 입니다.-----");
+		for(CoffeeDto menu: list) {
+			System.out.println(menu); // CoffeeDto.toStrin()호출하는 것
+		}
+	}
+
 }
