bdb64d5026266700c4a35cffaa319e1a7d30f20d
KiM-Taeseon <lion1445@daum.net>
2023-04-06 10:58:01 +0900

    Coffee_Order

--- KB_ITsYourLife_MiniProject/resources/schema/Coffee_Order.sql
+++ KB_ITsYourLife_MiniProject/resources/schema/Coffee_Order.sql
@@ -2,15 +2,12 @@
 
 --메뉴 테이블 (메뉴코드와 이름, HOT/ICE가격)
 CREATE TABLE tbl_menu(
-    beverage_code number(8) primary key,
-    beverage_name varchar2(50) unique,
-    hot_price number(8) not null,
-    ice_price number(8) not null
+    beverage_code number(8) primary key, --메뉴코드
+    beverage_name varchar2(50) unique,   --메뉴이름
+    hot_price number(8) not null,        --뜨거운 음료 가격
+    ice_price number(8) not null         --차가운 음료 가격
 );
 
-CREATE SEQUENCE seq_menu
-START WITH 1
-INCREMENT BY 1;
 
 INSERT INTO tbl_menu values(1, '아메리카노', 4500, 5000);
 INSERT INTO tbl_menu values(2, '카페라떼', 5500, 6000);
@@ -23,20 +20,24 @@ INSERT INTO tbl_menu values(8, '아포카토', 6500, 7000);
 INSERT INTO tbl_menu values(9, '초코라떼', 5800, 6300);
 
 SELECT * FROM tbl_menu;
-select * from tbl_menu where beverage_name LIKE '%카페%';
-select * from tbl_menu where beverage_name LIKE '%라떼%';
 
---주문 테이블 (주문번호, 주문아이디, 주문날짜)
+
+select * from tbl_menu where beverage_name LIKE '%카페%'; --확인용
+select * from tbl_menu where beverage_name LIKE '%라떼%'; --확인용
+
+
+--주문 테이블 (주문번호, 포장여부, 주문날짜, 총가격)
 CREATE TABLE tbl_order(
-    order_num_seq number(4) primary key,
-    G_H number(1) check(G_H in(1, 2)), --1이면 GO, 2이면 HERE  
-    order_date Date default sysdate,
-    total_price number(8)           -- SUM(each_price)
+    order_num_seq number(4) primary key,       --주문번호
+    is_togo number(1) check(is_togo in(1, 2)), --포장여부 : 1이면 GO, 2이면 HERE  
+    order_date Date default sysdate,           --주문날짜
+    total_price number(8)                      -- SUM(each_price)
 );
 
-CREATE SEQUENCE seq_order
+CREATE SEQUENCE seq_order           --시퀀스문 추가
 START WITH 1
 INCREMENT BY 1;
+
 
 INSERT INTO tbl_order(order_num_seq, order_date) VALUES(seq_order.NEXTVAL, DEFAULT);
 INSERT INTO tbl_order(order_num_seq, order_date) VALUES(seq_order.NEXTVAL, DEFAULT);
@@ -47,29 +48,35 @@ INSERT INTO tbl_order(order_num_seq, order_date) VALUES(seq_order.NEXTVAL, DEFAU
 SELECT * FROM tbl_order;
 
 
---주문상세 테이블(주문번호를 참조하기 위해 주문테이블의 orderNum을 FK 선언)
---(상세주문코드, 주문번호(FK), 메뉴번호(이름), HOT/ICE, 수량, 가격)
+--주문상세 테이블(주문번호를 참조하기 위해 주문테이블의 order_num_seq을 참조하는 order_num을 PK와 FK로 잡음)
+--(상세주문코드, 주문번호(FK), 메뉴이름, HOT/ICE, 수량, 각메뉴의 총가격)
 CREATE TABLE tbl_detail_order(
-    order_code number(4) primary key,
-    order_num number(2) constraint fk_seqorder REFERENCES tbl_order(order_num_seq),
-    menu_name varchar(20),
-    H_I number(1) check(H_I in(1, 2)), --1이면 HOT, 2이면 ICE
-    amount number(2),
-    each_price number(8)     --AMOUNT X PRICE   
+    order_code number(4) primary key,        --상세주문코드
+    order_num number(2) constraint fk_seqorder REFERENCES tbl_order(order_num_seq), --주문번호
+    menu_name varchar(20),                   --메뉴이름
+    is_hot number(1) check(is_hot in(1, 2)), --HOT/ICE 1이면 HOT, 2이면 ICE
+    amount number(2),                        --수량
+    each_price number(8)                     --각메뉴의 총가격 AMOUNT X PRICE   
 );
 
+CREATE SEQUENCE seq_code                     --시퀀스문 추가
+START WITH 1
+INCREMENT BY 1;
 
-INSERT INTO tbl_detail_order(order_code, order_num) VALUES(1, seq_order.currVAL);
-INSERT INTO tbl_detail_order(order_code, order_num) VALUES(2, seq_order.currVAL);
-INSERT INTO tbl_detail_order(order_code, order_num) VALUES(3, seq_order.currVAL);
-INSERT INTO tbl_detail_order(order_code, order_num) VALUES(4, seq_order.currVAL);
-INSERT INTO tbl_detail_order(order_code, order_num) VALUES(5, seq_order.currVAL);
+
+INSERT INTO tbl_detail_order(order_code, order_num) VALUES(seq_code.NEXTVAL, seq_order.CURRVAL);
+INSERT INTO tbl_detail_order(order_code, order_num) VALUES(seq_code.NEXTVAL, seq_order.CURRVAL);
+INSERT INTO tbl_detail_order(order_code, order_num) VALUES(seq_code.NEXTVAL, seq_order.CURRVAL);
+INSERT INTO tbl_detail_order(order_code, order_num) VALUES(seq_code.NEXTVAL, seq_order.CURRVAL);
+INSERT INTO tbl_detail_order(order_code, order_num) VALUES(seq_code.NEXTVAL, seq_order.CURRVAL);
 
 SELECT * FROM tbl_detail_order;
 
 commit; --테이블 생성되어있지만 COMMIT하지 않음. ROLLBACK 가능!
 rollback;
 
+DROP TABLE tbl_order;
+DROP SEQUENCE seq_code;
 
 
 
--- KB_ITsYourLife_MiniProject/src/cafe/dao/OrderDAOImpl.java
+++ KB_ITsYourLife_MiniProject/src/cafe/dao/OrderDAOImpl.java
@@ -2,181 +2,93 @@ package cafe.dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 
 import cafe.common.DBManager;
-import cafe.dto.CoffeeDto;
 import cafe.dto.OrderDetailDto;
 import cafe.dto.OrderDto;
-import cafe.exception.DMLException;
 
 public class OrderDAOImpl implements OrderDAO{
-
+	
 	private static OrderDAO instance = new OrderDAOImpl();
-
+	
 	/**
 	 * 외부에서 객체 생성 막음 (싱글톤)
 	 * */
 	private OrderDAOImpl() {}
 	public static OrderDAO getInstance() {
-		return instance; 
+		return instance;
 	}
+	
 
-
-	/**
-	 * 주문 insert 구현
-	 * @작성자 : 곽승규
-	 * */
 	@Override
-	public int orderInsert(OrderDto orderDto) throws DMLException {
+	public int orderInsert(OrderDto orderDto) throws SQLException {
 		Connection con = null;
 		PreparedStatement ps = null;
-
-		String sql = "INSERT INTO tbl_order(order_num_seq, is_togo, order_date) VALUES(seq_order.NEXTVAL, ?, DEFAULT)";
-
+		//ResultSet rs = null;
+		String sql = "INSERT INTO tbl_order(order_num_seq, G_H, order_date, total_price) VALUES(seq_order.NEXTVAL, ?, DEFAULT, ?)";
+		
 		int result = 0;
-
-
+		
+		
 		try {
-			con = DBManager.getConnection();
+			con =  DBManager.getConnection();
+			con.setAutoCommit(false);
 			
-			con.setAutoCommit(false); // 자동커밋 해지
-
 			ps = con.prepareStatement(sql);
-
-			ps.setInt(1, orderDto.getIsToGo());
+			
+			ps.setInt(1, orderDto.getOrderNumSeq());
+			ps.setInt(1, orderDto.getGH());
+			ps.setInt(1, orderDto.getTotalPrice());
+			ps.setInt(1, orderDto.getTotalPrice());
 			
 			result = ps.executeUpdate();
 			
-			
-			int re [] = this.orderDetails(con, orderDto);
-			
-			for(int i : re) {
-				if ( i != 1) {
-					con.rollback();
-					throw new DMLException("주문상세 입력에 예외가 발생했습니다.");
-				}
+			if (rs.next()) {
+				int re [] = this.orderDetails(con, orderDto);
 			}
-			
-			con.commit(); //커밋하기
-			System.out.println(orderDto);
 			
 		} catch (SQLException e) {
 			e.printStackTrace();
-			try {
-				con.rollback();
-			}catch (SQLException ex) {
-				ex.printStackTrace();
-			}
-			throw new DMLException("주문 입력에 예외가 발생했습니다.");
+			throw new SQLException("주문 입력에 예외가 발생했습니다.");
 		}finally {
 			DBManager.releaseConnection(con, ps);
 		}
 
 		return result;
-
+		
 	}
 
-	/**
-	 * 주문테이블에 total_price 업데이트하기
-	 * @작성자 : 곽승규
-	 * */
-	public int orderUpdate(OrderDto orderDto) throws DMLException {
-		Connection con = null;
-		PreparedStatement ps = null;
-		String sql = "update tbl_order set total_price = ? where order_num_seq = ?";
-		
-		int result = 0;
-		
-		try {
-			con = DBManager.getConnection();
-			ps = con.prepareStatement(sql);
-			
-			int totalPrice = getTotalPrice(orderDto); // 값 나오는 것 확인	
-			ps.setInt(1, orderDto.getTotalPrice()); // 총 금액 
-			ps.setInt(2, orderDto.getOrderNumSeq()); //여기에서 문제가생긴듯 함.
-			
-			result = ps.executeUpdate();
-			
-		}catch(SQLException e) {
-			e.printStackTrace();
-			throw new DMLException("토탈 금액 수정에 예외가 발생했습니다.");
-		}
-		finally {
-			DBManager.releaseConnection(con, ps);
-		}
-		
-		return result;
-	}
-	
-	
-	
-	/**
-	 * 주문상세 insert 구현
-	 * @작성자 : 곽승규
-	 * */
 	@Override
 	public int[] orderDetails(Connection con, OrderDto orderDto) throws SQLException {
 		PreparedStatement ps = null;
 
-		String sql = "insert into tbl_detail_order values(order_detail_seq.NEXTVAL, seq_order.CURRVAL, ?, ?, ?, ?)";
-
+		String sql = "insert into tbl_detail_order values( ?, seq_order.CURRVAL, ?, ?, ?)";
+		
 		int result [] = null;
 		int count = 0;
 		try {
 			ps = con.prepareStatement(sql);
-
+			
 			List<OrderDetailDto> orderDetailList = orderDto.getOrderDetailList();
 			result = new int[orderDetailList.size()];
-			int totalPrice = 0; // 토탈 금액 저장할 변수
-			
 			for( OrderDetailDto orderDetail : orderDetailList) {
+				ps.setInt(1, orderDetail.getOrderDetailCode());
+				ps.setString(2, orderDetail.getMenuName());
+				ps.setInt(3, orderDetail.getMenuEA());
+				ps.setInt(4, orderDetail.getKind());
 				
-				// 각 튜플마다 가격 넣기 위한 코드
-				CoffeeDAO coffeeDAO = CoffeeDAOImpl.getInstance();
-				CoffeeDto beverage = coffeeDAO.coffeeSelectByName(orderDetail.getMenuName());
-				
-				ps.setString(1, orderDetail.getMenuName());
-				ps.setInt(2, orderDetail.getKind());
-				ps.setInt(3, orderDetail.getAmount());
-				
-				int price = 0; // 주문상세 당 각각의 가격 저장할 변수
-				
-				
-				if (orderDetail.getKind() == 1) { // hot임
-					price = beverage.getHotPrice() * orderDetail.getAmount();
-				}else {//ice임
-					price = beverage.getIcePrice() * orderDetail.getAmount();
-				}
-				orderDetail.setEachPrice(price);
-				
-				ps.setInt(4, price);
-				
-				totalPrice += price;
-
 				result[count++] = ps.executeUpdate();
-
+				
 			}
 			
-			// 총 금액 저장하기
-			orderDto.setTotalPrice(totalPrice);
-
 		} finally {
 			DBManager.releaseConnection(null,ps);
 		}
 		return result;
 	}
 
-	public int getTotalPrice(OrderDto orderDto) {
-		List<OrderDetailDto> orderDetailList = orderDto.getOrderDetailList();
-		
-		int totalPrice = 0;
-		for( OrderDetailDto orderDetail : orderDetailList) {
-			int tmp = orderDetail.getEachPrice();
-			totalPrice += tmp;
-		}
-		return totalPrice;
-	}
 
 }
