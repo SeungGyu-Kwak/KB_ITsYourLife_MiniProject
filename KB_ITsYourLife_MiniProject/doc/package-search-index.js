845d4db654b8cf4ff5fc1d1ea1f66e8100329fdb
Gyu <tmdrb0912@naver.com>
2023-04-06 17:08:02 +0900

    fix : java docs 제거

--- KB_ITsYourLife_MiniProject/doc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"cafe.common"},{"l":"cafe.controller"},{"l":"cafe.dao"},{"l":"cafe.dto"},{"l":"cafe.exception"},{"l":"cafe.service"},{"l":"cafe.view"},{"l":"test"}]
